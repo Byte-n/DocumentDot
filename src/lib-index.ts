367ebea0272f07bddf9ceefbfb1bc1ba532d1149
byte <dancingbytes@qq.com>
2023-05-10 20:01:59 +0800

    #FEAT:version 0.0.7 修改导出 Types

--- src/lib-index.ts
+++ src/lib-index.ts
@@ -1,6 +1,6 @@
 import DocumentDot from "./script/DocumentDot";
 import Dot from "./script/Dot";
-export * as Types from './script/Type'
+export * from './script/Type'
 // @ts-ignore
 import ImageTools from './script/ImageTools.js'
 export {
